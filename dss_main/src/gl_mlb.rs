40f643f65e14b44dd18497689e43de6f412dcb7a
eta077 <eta077@yahoo.com>
2020-09-07 21:40:51 -0500

    Added border to focused game

--- dss_main/src/gl_mlb.rs
+++ dss_main/src/gl_mlb.rs
@@ -1,10 +1,11 @@
 //! OpenGL implementation of the MLB UI.
 
 use crate::gl_utils;
-use crate::gl_utils::{FocusDirection, ImageVertex};
+use crate::gl_utils::{FocusDirection, ImageVertex, Vertex};
 use dss_mlb::MlbGameClientInfo;
+use glium::index::{NoIndices, PrimitiveType};
 use glium::texture::{RawImage2d, Texture2d};
-use glium::{Display, Frame, Program, Surface, VertexBuffer};
+use glium::{Display, DrawParameters, Frame, Program, Surface, VertexBuffer};
 use glium_glyph::glyph_brush::Section;
 use glium_glyph::GlyphBrush;
 use log::error;
@@ -13,6 +14,12 @@ use log::error;
 const DEFAULT_RAW: &[u8; 22931] = include_bytes!("default.jpg");
 /// The number of games to display at a time for each day.
 const X_PAGE_SIZE: usize = 5;
+/// The percentage of the screen taken up by the border of a focused game.
+const BORDER_SCALE: f32 = 0.175;
+/// The percentage of the screen taken up by a line in the border of a focused game.
+const BORDER_LINE_SCALE: f32 = 0.015;
+/// The percentage of the screen for horizontal and vertical padding from the focused game image.
+const BORDER_OFFSET: f32 = 0.025;
 /// The percentage from the left of the screen at which to start displaying game images.
 const LEFT_INDENT: f32 = 0.05;
 /// The percentage from the top of the screen at which to start displaying game images.
@@ -31,9 +38,11 @@ const NON_FOCUSED_OFFSET: f32 = 0.025;
 /// The manager of the MLB UI responsible for rendering implementation and ownership of the backing data.
 pub struct MlbGlUi {
     ui_info: MlbUiInfo,
-    program: Program,
+    image_program: Program,
     image_square_vertices: VertexBuffer<ImageVertex>,
     background_texture: Texture2d,
+    rect_program: Program,
+    border_vertices: VertexBuffer<Vertex>,
 }
 
 impl MlbGlUi {
@@ -42,14 +51,14 @@ impl MlbGlUi {
     /// # Errors
     /// Panics if the given display cannot be used to create UI elements.
     pub fn init(ui_info: MlbUiInfo, display: &Display) -> Self {
-        let program = Program::from_source(
+        let image_program = Program::from_source(
             display,
             gl_utils::IMAGE_VERTEX_SHADER_SRC,
             gl_utils::IMAGE_FRAGMENT_SHADER_SRC,
             None,
         )
         .unwrap_or_else(|ex| {
-            let msg = "Could not create OpenGL Program";
+            let msg = "Could not create OpenGL image program";
             error!("{}:\n{}", msg, ex);
             panic!("{}.", msg);
         });
@@ -63,20 +72,12 @@ impl MlbGlUi {
                 tex_coords: [0.0, 1.0],
             },
             ImageVertex {
-                position: [1.0, 1.0],
-                tex_coords: [1.0, 1.0],
-            },
-            ImageVertex {
-                position: [-1.0, -1.0],
-                tex_coords: [0.0, 0.0],
-            },
-            ImageVertex {
-                position: [1.0, 1.0],
-                tex_coords: [1.0, 1.0],
-            },
-            ImageVertex {
                 position: [1.0, -1.0],
                 tex_coords: [1.0, 0.0],
+            },
+            ImageVertex {
+                position: [1.0, 1.0],
+                tex_coords: [1.0, 1.0],
             },
         ];
         let image_square_vertices = VertexBuffer::new(display, &image_square_shape).unwrap_or_else(|ex| {
@@ -99,11 +100,131 @@ impl MlbGlUi {
             error!("{}:\n{}", msg, ex);
             panic!("{}.", msg);
         });
+        let rect_program = Program::from_source(
+            display,
+            gl_utils::RECT_VERTEX_SHADER_SRC,
+            gl_utils::RECT_FRAGMENT_SHADER_SRC,
+            None,
+        )
+        .unwrap_or_else(|ex| {
+            let msg = "Could not create OpenGL rect program";
+            error!("{}:\n{}", msg, ex);
+            panic!("{}.", msg);
+        });
+        let border_shape = vec![
+            // left edge
+            Vertex {
+                position: [-1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0 + BORDER_LINE_SCALE * 2.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0 + BORDER_LINE_SCALE * 2.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0 + BORDER_LINE_SCALE * 2.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            // top edge
+            Vertex {
+                position: [-1.0, 1.0 - BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0 - BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, 1.0 - BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            // right edge
+            Vertex {
+                position: [1.0 - BORDER_LINE_SCALE * 2.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0 - BORDER_LINE_SCALE * 2.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, 1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0 - BORDER_LINE_SCALE * 2.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            // bottom edge
+            Vertex {
+                position: [-1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, -1.0 + BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0 + BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [-1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0 + BORDER_LINE_SCALE * 2.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+            Vertex {
+                position: [1.0, -1.0],
+                color: [0.5, 0.5, 0.5, 1.0],
+            },
+        ];
+        let border_vertices = VertexBuffer::new(display, &border_shape).unwrap_or_else(|ex| {
+            let msg = "Could not create border vertices";
+            error!("{}:\n{}", msg, ex);
+            panic!("{}.", msg);
+        });
         MlbGlUi {
             ui_info,
-            program,
+            image_program,
             image_square_vertices,
             background_texture,
+            rect_program,
+            border_vertices,
         }
     }
 
@@ -112,6 +233,10 @@ impl MlbGlUi {
     /// # Errors
     /// Panics if the given target cannot be used to render the MLB UI.
     pub fn draw(&mut self, display: &Display, target: &mut Frame, text_brush_option: Option<&mut GlyphBrush>) {
+        let screen_dims = display.get_framebuffer_dimensions();
+        let screen_width = screen_dims.0 as f32;
+        let screen_height = screen_dims.1 as f32;
+
         let background_uniforms = uniform! {
             matrix: [
                 [1.0, 0.0, 0.0, 0.0],
@@ -124,10 +249,10 @@ impl MlbGlUi {
         target
             .draw(
                 &self.image_square_vertices,
-                &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
-                &self.program,
+                &NoIndices(PrimitiveType::TriangleStrip),
+                &self.image_program,
                 &background_uniforms,
-                &Default::default(),
+                &DrawParameters::default(),
             )
             .unwrap_or_else(|ex| {
                 let msg = "Target could not draw background";
@@ -137,6 +262,32 @@ impl MlbGlUi {
 
         let focused_day = self.ui_info.focused_day;
         let focused_index = self.ui_info.focused_index;
+        let (focused_translate_x, focused_translate_y) =
+            calc_game_location_percentage(true, focused_index as f32, focused_day as f32);
+        let x_offset = -1.0 + (focused_translate_x + BORDER_SCALE / 2.0) * 2.0 - BORDER_OFFSET;
+        let y_offset = 1.0 - (focused_translate_y + BORDER_SCALE / 2.0) * 2.0 + BORDER_OFFSET;
+        let border_uniforms = uniform! {
+            matrix: [
+                [BORDER_SCALE, 0.0, 0.0, 0.0],
+                [0.0, BORDER_SCALE, 0.0, 0.0],
+                [0.0, 0.0, BORDER_SCALE, 0.0],
+                [x_offset, y_offset, 0.0, 1.0f32],
+            ]
+        };
+        target
+            .draw(
+                &self.border_vertices,
+                &NoIndices(PrimitiveType::TrianglesList),
+                &self.rect_program,
+                &border_uniforms,
+                &DrawParameters::default(),
+            )
+            .unwrap_or_else(|ex| {
+                let msg = "Target could not draw selected border";
+                error!("{}:\n{}", msg, ex);
+                panic!("{}.", msg);
+            });
+
         for (row, day) in self.ui_info.days.iter_mut().enumerate() {
             for i in day.begin_index..(day.begin_index + X_PAGE_SIZE) {
                 let col = i - day.begin_index;
@@ -154,7 +305,7 @@ impl MlbGlUi {
                     (game_scale, translate_x, translate_y)
                 };
 
-                let x_offset = (translate_x + game_scale / 2.0) * 2.0 - 1.0;
+                let x_offset = -1.0 + (translate_x + game_scale / 2.0) * 2.0;
                 let y_offset = 1.0 - (translate_y + game_scale / 2.0) * 2.0;
                 let game_uniforms = uniform! {
                     matrix: [
@@ -168,10 +319,10 @@ impl MlbGlUi {
                 target
                     .draw(
                         &self.image_square_vertices,
-                        &glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList),
-                        &self.program,
+                        &NoIndices(PrimitiveType::TriangleStrip),
+                        &self.image_program,
                         &game_uniforms,
-                        &Default::default(),
+                        &DrawParameters::default(),
                     )
                     .unwrap_or_else(|ex| {
                         let msg = "Target could not draw game";
@@ -181,16 +332,10 @@ impl MlbGlUi {
             }
         }
         if let Some(text_brush) = text_brush_option {
-            let screen_dims = display.get_framebuffer_dimensions();
-            let screen_width = screen_dims.0 as f32;
-            let screen_height = screen_dims.1 as f32;
-
             let focused_day_info = &self.ui_info.days[focused_day];
             let focused_game = &focused_day_info.games[focused_index + focused_day_info.begin_index].info;
-            let (translate_x, translate_y) =
-                calc_game_location_percentage(true, focused_index as f32, focused_day as f32);
-            let x_offset = translate_x * screen_width;
-            let y_offset = (translate_y - 0.05) * screen_height;
+            let x_offset = focused_translate_x * screen_width;
+            let y_offset = (focused_translate_y - 0.05) * screen_height;
             let text_top_left = (x_offset, y_offset);
             text_brush.queue(Section {
                 text: &focused_game.title,
@@ -198,8 +343,8 @@ impl MlbGlUi {
                 screen_position: text_top_left,
                 ..Section::default()
             });
-            let x_offset = translate_x * screen_width;
-            let y_offset = (translate_y + FOCUSED_GAME_SCALE) * screen_height;
+            let x_offset = focused_translate_x * screen_width;
+            let y_offset = (focused_translate_y + FOCUSED_GAME_SCALE + 0.025) * screen_height;
             let text_top_left = (x_offset, y_offset);
             text_brush.queue(Section {
                 text: &focused_game.summary,
--- dss_main/src/gl_utils.rs
+++ dss_main/src/gl_utils.rs
@@ -2,34 +2,35 @@
 
 /// The vertex shader program used to render an image.
 pub const IMAGE_VERTEX_SHADER_SRC: &str = r#"
-        #version 140
+    #version 140
 
-        uniform mat4 matrix;
+    uniform mat4 matrix;
 
-        in vec2 position;
-        in vec2 tex_coords;
+    in vec2 position;
+    in vec2 tex_coords;
 
-        out vec2 v_tex_coords;
-                
-        void main() {
-            v_tex_coords = tex_coords;
-            gl_Position = matrix * vec4(position, 0.0, 1.0);
-        }
-    "#;
+    out vec2 v_tex_coords;
+            
+    void main() {
+        v_tex_coords = tex_coords;
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+    }
+"#;
 
 /// The fragment shader program used to render an image.
 pub const IMAGE_FRAGMENT_SHADER_SRC: &str = r#"
-        #version 140
+    #version 140
 
-        uniform sampler2D tex;
+    uniform sampler2D tex;
 
-        in vec2 v_tex_coords;
-        out vec4 color;
-        
-        void main() {
-            color = texture(tex, v_tex_coords);
-        }
-    "#;
+    in vec2 v_tex_coords;
+
+    out vec4 color;
+    
+    void main() {
+        color = texture(tex, v_tex_coords);
+    }
+"#;
 
 /// A container for the position of a vertex and the associated texture.
 #[derive(Copy, Clone)]
@@ -38,6 +39,44 @@ pub struct ImageVertex {
     pub tex_coords: [f32; 2],
 }
 implement_vertex!(ImageVertex, position, tex_coords);
+
+/// The vertex shader program used to render a point with a color.
+pub const RECT_VERTEX_SHADER_SRC: &str = r#"
+    #version 140
+
+    uniform mat4 matrix;
+
+    in vec2 position;
+    in vec4 color;
+
+    out vec4 f_color;
+
+    void main() {
+        f_color = color;
+        gl_Position = matrix * vec4(position, 0.0, 1.0);
+    }
+"#;
+
+/// The fragment shader program used to apply the color of a vertex.
+pub const RECT_FRAGMENT_SHADER_SRC: &str = r#"
+    #version 140
+    
+    in vec4 f_color;
+
+    out vec4 color;
+
+    void main() {
+        color = f_color;
+    }
+"#;
+
+/// A container for the position and color of a vertex.
+#[derive(Copy, Clone)]
+pub struct Vertex {
+    pub position: [f32; 2],
+    pub color: [f32; 4],
+}
+implement_vertex!(Vertex, position, color);
 
 /// An enumeration of directions in which focus can move.
 pub enum FocusDirection {
